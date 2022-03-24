fb63af2d12fa922c73b3c7f781c4fab5365f7400
Mdmercado <marcosmerk156@gmail.com>
2022-03-24 19:34:59 -0300

    other changes in App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MaTienda</h1>
-      </header>
-      <footer>
-        Marcos Mercado web developer {"</>"}
-      </footer>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+        </header>
+        <body className='App-body'>
+        </body>
+      </div>
+    </>
+
   );
 }
 
