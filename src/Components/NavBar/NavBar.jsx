10d4892f7a6443c348a2391a6b3b614583b7af33
Mdmercado <marcosmerk156@gmail.com>
2022-03-31 11:43:59 -0300

    separate in the component folder navbar

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
+import CartWidget from "../Cartwidget/CartWidget";
+import "./NavBar.css";
+
+function NavBar() {
+	return (
+		<Navbar variant="dark" bg="dark" expand="lg">
+			<Container className="justify-content-center">
+				<Navbar.Brand href="#inicio">
+					<div className="navbar-logo"></div>
+				</Navbar.Brand>
+				<Navbar.Brand href="#inicio" className="navbar-title">
+					MaTienda
+				</Navbar.Brand>
+				<Navbar.Brand href="#cart" className="navbar-cart">
+					<CartWidget />
+				</Navbar.Brand>
+				<NavbarToggle aria-controls="nav-collapse" />
+				<NavbarCollapse id="nav-collapse" className="justify-content-center">
+					<Nav as="ul">
+						<Nav.Item as="li">
+							<Nav.Link href="#nosotros">Quiénes Somos</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<NavDropdown
+								title="PRODUCTOS"
+								id="basic-nav-dropdown"
+								className="bg-black">
+								<NavDropdown.Item href="#mates" className="dropdown-item">
+									Mates
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#yerbas" className="dropdown-item">
+									Yerbas Importadas
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#termos" className="dropdown-item">
+									Termos
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#materos" className="dropdown-item">
+									Materos
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav.Item>
+						<Nav.Item as="li">
+							<Nav.Link href="#comprar">Cómo Comprar</Nav.Link>
+						</Nav.Item>
+						<Nav.Item as="li">
+							<Nav.Link href="#contacto">Contacto</Nav.Link>
+						</Nav.Item>
+					</Nav>
+				</NavbarCollapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default NavBar;
