aa03ac2ea237fcdff4d48ff2dedfe76acf094d4c
Mdmercado <marcosmerk156@gmail.com>
2022-04-05 16:13:03 -0300

    add state and new props to components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@
 import './App.css';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import NavBar from './Components/NavBar/NavBar';
+import { useState } from 'react';
 
 function App() {
+  const [cartNumber, setCartNumber] = useState(0)
+
   return (
     <>
       <div className="App">
         <div className='App-header'>
-          <NavBar />
+          <NavBar cant={cartNumber} />
         </div>
         <div className='App-main'>
-          <ItemListContainer greeting="Hola Producto" />
+          <ItemListContainer greeting="Hola Producto" setCartCant={setCartNumber} />
         </div>
       </div>
     </>
