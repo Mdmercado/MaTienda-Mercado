42ba2166973caf186f25810d4764166cf2530485
Mdmercado <marcosmerk156@gmail.com>
2022-04-05 16:14:12 -0300

    add amount to icon

--- src/Components/Cartwidget/CartWidget.jsx
+++ src/Components/Cartwidget/CartWidget.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { FaCartPlus } from "react-icons/fa";
 import "../Cartwidget/CartWidget.css";
 
-function CartWidget() {
+function CartWidget({ cant }) {
 	return (
 		<div className="faCart">
 			<FaCartPlus className="faCart--icon" />
+			<p>{cant}</p>
 		</div>
 	);
 }
