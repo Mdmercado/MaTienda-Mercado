33f6b50dfdb837733d3066d9e488fa0cdbe63580
Mdmercado <marcosmerk156@gmail.com>
2022-04-07 19:31:25 -0300

    adjusting the component ItemListContainer to the request of the challenge

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,24 +1,33 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import "../ItemListContainer/ItemListContainer.css";
-import ItemCount from "../ItemCount/ItemCount";
-function ItemListContainer({ greeting, setCartCant }) {
+import productos from "../../Utils/productos.js";
+import ItemList from "../ItemList/ItemList";
+
+function ItemListContainer({ setCartCant }) {
 	const onAdd = (items) => {
 		setCartCant(items);
 		console.log("agregando " + items + " productos");
 	};
+
+	const [productsState, setProductsState] = useState([]);
+
+	useEffect(() => {
+		const llenar = new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve(productos);
+			}, 2000);
+		});
+
+		llenar.then((res) => setProductsState(res));
+		llenar.catch((error) => console.log(error));
+	}, [productsState]);
 
 	return (
 		<div className="container">
 			<div className="items-title">
 				<h1>Productos Destacados</h1>
 			</div>
-			<div className="item">
-				<p className="item--paragraph">{greeting}</p>
-				<div className="item--Amount">
-					<ItemCount stock={5} initial={1} onAdd={onAdd} />
-				</div>
-			</div>
+			<ItemList onAdd={onAdd} productos={productsState} />
 		</div>
 	);
 }
