430c301b6358d2ba24c9f785e2a0ce02fb8d390c
Mdmercado <marcosmerk156@gmail.com>
2022-04-19 11:28:03 -0300

    fix navigate error in Item

--- src/Components/ItemProduct/Item.jsx
+++ src/Components/ItemProduct/Item.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../ItemProduct/item.css";
 function Item({ id, nombre, precio, imagen, onAdd }) {
-	const navigate = useNavigate();
-
 	return (
 		<div className="item m-5">
 			<Card>
@@ -13,14 +11,12 @@ function Item({ id, nombre, precio, imagen, onAdd }) {
 					<Card.Title>{nombre}</Card.Title>
 					<Card.Text className="text-center">{precio}</Card.Text>
 				</Card.Body>
-				<Card.Footer>
-					<Button
-						onClick={() => {
-							navigate(`item/${id}`);
-						}}>
-						{" "}
-						Ir a Detalle
-					</Button>
+				<Card.Footer className="text-center">
+					<Link className="footer-link" to={`/item/${id}`}>
+						<Button size="lg" variant="outline-primary">
+							{"Ir a Detalle "}
+						</Button>
+					</Link>
 				</Card.Footer>
 			</Card>
 		</div>
