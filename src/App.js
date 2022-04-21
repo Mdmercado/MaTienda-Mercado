3df203797f4fe54d39b037921cfc967df45052ec
Mdmercado <marcosmerk156@gmail.com>
2022-04-21 14:55:09 -0300

    add contextProvider to app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer'
 import Cart from './Components/Cart.jsx'
 import NavBar from './Components/NavBar/NavBar';
+import CartContexProvider from './Context/cartContext';
 import { useState } from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
@@ -13,14 +14,15 @@ function App() {
   const [cartNumber, setCartNumber] = useState(0)
   return (
     <BrowserRouter>
-      <NavBar cant={cartNumber} />
-
-      <Routes>
-        <Route exact path="/" element={<ItemListContainer setCartCant={setCartNumber} />} />
-        <Route exact path='/category/:categoryId' element={<ItemListContainer />} />
-        <Route exact path='/item/:id' element={<ItemDetailContainer />} />
-        <Route exact path='/cart' element={<Cart />} />
-      </Routes>
+      <CartContexProvider>
+        <NavBar cant={cartNumber} />
+        <Routes>
+          <Route exact path="/" element={<ItemListContainer setCartCant={setCartNumber} />} />
+          <Route exact path='/category/:categoryId' element={<ItemListContainer />} />
+          <Route exact path='/item/:id' element={<ItemDetailContainer />} />
+          <Route exact path='/cart' element={<Cart />} />
+        </Routes>
+      </CartContexProvider>
     </BrowserRouter>
   );
 }
