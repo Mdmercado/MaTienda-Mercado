6478793d2dbef57e01199472f47f35d62d091c59
Mdmercado <marcosmerk156@gmail.com>
2022-04-28 20:54:40 -0300

    feat: using information extracted from the database

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,31 +1,25 @@
 import React, { useState, useEffect } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
-import { productos } from "../../Utils/productos.js";
+
 import { useParams } from "react-router-dom";
 import { Spinner } from "react-bootstrap";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 export default function ItemDetailContainer() {
 	const { id } = useParams();
 	const [producto, setproducto] = useState([]);
 	const [loading, setLoading] = useState(false);
 
-	const getOneProduct = (id) => {
-		return new Promise((resolve, reject) => {
-			const productoElegido = productos.find((p) => p.id === Number(id));
-			setTimeout(() => {
-				resolve(productoElegido);
-			}, 2000);
-		});
-	};
-
 	useEffect(() => {
 		setLoading(true);
-		getOneProduct(id)
-			.then((res) => setproducto(res))
-			.catch((error) => console.log(error))
-			.finally(() => {
+		const db = getFirestore();
+		const itemref = doc(db, "productos", `${id}`);
+		getDoc(itemref).then((result) => {
+			if (result.exists()) {
+				setproducto({ id: result.id, ...result.data() });
 				setLoading(false);
-			});
+			}
+		});
 	}, [id]);
 
 	return (
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,34 +1,41 @@
 import React, { useEffect, useState } from "react";
 import "../ItemListContainer/ItemListContainer.css";
-import { productos } from "../../Utils/productos.js";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
 import { Spinner } from "react-bootstrap";
 import "../ItemListContainer/ItemListContainer.css";
+import {
+	collection,
+	getDocs,
+	getFirestore,
+	query,
+	where,
+} from "firebase/firestore";
 
 function ItemListContainer() {
 	const [productsState, setProductsState] = useState([]);
-	const [loading, setsetLoading] = useState();
-
+	const [loading, setLoading] = useState();
 	const { categoryId } = useParams();
 
-	const getProductos = (categoryId) => {
-		return new Promise((resolve, reject) => {
-			const filtro = productos.filter((p) => p.category === categoryId);
-			setTimeout(() => {
-				categoryId ? resolve(filtro) : resolve(productos);
-			}, 2000);
-		});
-	};
-
 	useEffect(() => {
-		setsetLoading(true);
-		getProductos(categoryId)
-			.then((res) => setProductsState(res))
-			.catch((error) => console.log(error))
-			.finally(() => {
-				setsetLoading(false);
-			});
+		setLoading(true);
+		const db = getFirestore();
+		let itemCollection;
+
+		if (!categoryId) {
+			itemCollection = collection(db, "productos");
+		} else {
+			itemCollection = query(
+				collection(db, "productos"),
+				where("categoryId", "==", `${categoryId}`)
+			);
+		}
+		getDocs(itemCollection).then((result) => {
+			setProductsState(
+				result.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+			);
+			setLoading(false);
+		});
 	}, [categoryId]);
 
 	return (
