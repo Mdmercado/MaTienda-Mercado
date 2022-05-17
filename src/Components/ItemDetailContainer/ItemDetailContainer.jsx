d6741d445487ad76488c0ebcbdea92c5520c0ed0
Mdmercado <marcosmerk156@gmail.com>
2022-05-16 22:11:39 -0300

    feat: adding scrollTOP

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -6,33 +6,34 @@ import { Spinner } from "react-bootstrap";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 export default function ItemDetailContainer() {
-	const { id } = useParams();
-	const [producto, setproducto] = useState([]);
-	const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const [producto, setproducto] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		setLoading(true);
-		const db = getFirestore();
-		const itemref = doc(db, "productos", `${id}`);
-		getDoc(itemref).then((result) => {
-			if (result.exists()) {
-				setproducto({ id: result.id, ...result.data() });
-				setLoading(false);
-			}
-		});
-	}, [id]);
+  useEffect(() => {
+    window.scrollTo(500, 0);
+    setLoading(true);
+    const db = getFirestore();
+    const itemref = doc(db, "productos", `${id}`);
+    getDoc(itemref).then((result) => {
+      if (result.exists()) {
+        setproducto({ id: result.id, ...result.data() });
+        setLoading(false);
+      }
+    });
+  }, [id]);
 
-	return (
-		<>
-			{loading ? (
-				<div className="carga">
-					<Spinner animation="border" role="status" variant="secondary">
-						<span className="visually-hidden">Loading...</span>
-					</Spinner>
-				</div>
-			) : (
-				<ItemDetail producto={producto} />
-			)}
-		</>
-	);
+  return (
+    <>
+      {loading ? (
+        <div className="carga" style={{ height: "800px" }}>
+          <Spinner animation="border" role="status" variant="secondary">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <ItemDetail producto={producto} />
+      )}
+    </>
+  );
 }
