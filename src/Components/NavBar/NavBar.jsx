3ed9baa9fd10cbc0ddc03916a72b1d411a36c229
Mdmercado <marcosmerk156@gmail.com>
2022-05-16 22:15:30 -0300

    finalizing navBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
@@ -7,71 +7,76 @@ import { Link } from "react-router-dom";
 import { CartContext } from "../../Context/cartContext";
 import CartWidget from "../Cartwidget/CartWidget";
 import "./NavBar.css";
-
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 function NavBar() {
-	const { productsAdd, showTotal } = useContext(CartContext);
+  const { productsAdd, showTotal, showcart } = useContext(CartContext);
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    showTotal();
+  }, [productsAdd]);
 
-	useEffect(() => {
-		showTotal();
-	}, [productsAdd]);
-	return (
-		<Navbar className="mb-5" variant="dark" bg="dark" expand="lg">
-			<Container className="justify-content-center">
-				<Navbar.Brand as={Link} to="/">
-					<div className="navbar-logo"></div>
-				</Navbar.Brand>
-				<Navbar.Brand as={Link} to="/" className="navbar-title">
-					MaTienda
-				</Navbar.Brand>
-				{productsAdd.length > 0 && (
-					<Navbar.Brand as={Link} to="/cart" className="navbar-cart">
-						<CartWidget />
-					</Navbar.Brand>
-				)}
-				<NavbarToggle aria-controls="nav-collapse" />
-				<NavbarCollapse id="nav-collapse" className="justify-content-center">
-					<Nav as="ul">
-						<Nav.Item as="li">
-							<Nav.Link as={Link} to="/">
-								inicio
-							</Nav.Link>
-						</Nav.Item>
-						<Nav.Item>
-							<NavDropdown
-								title="PRODUCTOS"
-								id="basic-nav-dropdown"
-								className="bg-black">
-								<NavDropdown.Item
-									as={Link}
-									to="/category/mate"
-									className="dropdown-item">
-									Mates
-								</NavDropdown.Item>
-								<NavDropdown.Item
-									as={Link}
-									to="/category/bombilla"
-									className="dropdown-item">
-									Bombillas
-								</NavDropdown.Item>
-								<NavDropdown.Item
-									as={Link}
-									to="/category/termo"
-									className="dropdown-item">
-									Termos
-								</NavDropdown.Item>
-							</NavDropdown>
-						</Nav.Item>
-						<Nav.Item as="li">
-							<Nav.Link href="#comprar">Cómo Comprar</Nav.Link>
-						</Nav.Item>
-						<Nav.Item as="li">
-							<Nav.Link href="#contacto">Contacto</Nav.Link>
-						</Nav.Item>
-					</Nav>
-				</NavbarCollapse>
-			</Container>
-		</Navbar>
-	);
+  useEffect(() => {
+    const db = getFirestore();
+    const itemCollection = collection(db, "categorias");
+    getDocs(itemCollection).then((item) => {
+      setCategory(
+        item.docs.map((doc) => ({ name: doc.data.name, ...doc.data() }))
+      );
+    });
+  }, [showcart]);
+
+  return (
+    <Navbar className="mb-3" variant="dark" bg="dark" expand="lg">
+      <Container className="justify-content-center">
+        <Navbar.Brand as={Link} to="/">
+          <div className="navbar-logo"></div>
+        </Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="navbar-title">
+          MaTienda
+        </Navbar.Brand>
+        <Navbar.Brand as={Link} to="/cart" className="navbar-cart">
+          {showcart && <CartWidget />}
+        </Navbar.Brand>
+        <NavbarToggle aria-controls="nav-collapse" />
+        <NavbarCollapse id="nav-collapse" className="justify-content-center">
+          <Nav as="ul">
+            <Nav.Item as="li">
+              <Nav.Link as={Link} to="/">
+                inicio
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <NavDropdown
+                title="PRODUCTOS"
+                id="basic-nav-dropdown"
+                className="bg-black"
+              >
+                {category &&
+                  category.map((cat) => (
+                    <NavDropdown.Item
+                      as={Link}
+                      to={`/category/${cat.name}`}
+                      className="dropdown-item"
+                      key={cat.id}
+                    >
+                      {cat.name}s
+                    </NavDropdown.Item>
+                  ))}
+              </NavDropdown>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link as={Link} to={"/howtobuy"}>
+                Cómo Comprar
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link href="#contacto">Contacto</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </NavbarCollapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
